1dba15bb7b878ea44c095f81a42b44d70e3961a3
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2024-05-06 22:29:54 -0400

    Add example of execution crud operations

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,15 @@ async fn main() {
 
     let product = Product {
         id: None,
-        name: "Shoes101".to_string(),
+        name: "Shoes102".to_string(),
         description: "This is the sports shoes".to_string(),
-        price: 10.0,
-        quantity: 9000
+        price: 15.0,
+        quantity: 8000
     };
 
     let result = repository.create_product(product).await.unwrap();
+    //let result = repository.get_by_id(String::from("ID_GENERATED_AFTER_CREATE")).await.unwrap();
+    //let result = repository.update_product(String::from("ID_GENERATED_AFTER_CREATE"), product).await.unwrap();
 
     println!("{:?}", result);
 }
