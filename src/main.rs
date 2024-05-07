796de465d6d36b1b69ad07b9def58eb5869db4e1
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2024-05-06 22:25:13 -0400

    Add example of execution crud

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use crud_surrealdb::model::Product;
+use crud_surrealdb::product_repository::ProductRepository;
+
+#[tokio::main]
+async fn main() {
+    let repository = ProductRepository::new().await;
+
+    let product = Product {
+        id: None,
+        name: "Shoes101".to_string(),
+        description: "This is the sports shoes".to_string(),
+        price: 10.0,
+        quantity: 9000
+    };
+
+    let result = repository.create_product(product).await.unwrap();
+
+    println!("{:?}", result);
 }
